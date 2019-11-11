ab4e8964d8dd6eaa01c35636a7c92f5b5832b16a
Mohammad Banisaeid <mohammad@eclecticiq.com>
2019-11-11 11:34:53 +0100

    Update the brew upate scripts

--- osx/update-brew-list.sh
+++ osx/update-brew-list.sh
@@ -1,3 +1,21 @@
-#!/bin/bash
+set -eu
 
-brew bundle dump --force
+original_file="Brewfile"
+temp_file="temp_file"
+output_file="result"
+
+echo "Dumping current brew..."
+brew bundle dump --force --file=$temp_file
+rm -f $output_file
+
+echo "Merging..."
+cat $original_file $temp_file | grep "tap \"" | sort | uniq >> $output_file
+cat $original_file $temp_file | grep "brew \"" | sort | uniq >> $output_file
+cat $original_file $temp_file | grep "cask \"" | sort | uniq >> $output_file
+
+rm $temp_file
+
+echo "Replacing with the original file..."
+mv $output_file $original_file
+
+echo "Done."
