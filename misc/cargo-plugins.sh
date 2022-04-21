d9a4a978526a688c4447a5088284b95bc6945954
Mohammad Banisaeid <smbl64@gmail.com>
2022-04-21 16:12:49 +0200

    [rust] Add new plugins and components

--- misc/cargo-plugins.sh
+++ misc/cargo-plugins.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-cargo install cargo-tarpaulin \
+cargo install \
+    cargo-tarpaulin \
     cargo-edit \
     cargo-expand \
     cargo-watch \
     cargo-audit \
     cargo-udeps \
+    cargo-cache
+
+rustup component add \
+    rustfmt \
+    clippy
