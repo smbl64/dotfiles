d120cafb8bf7627dbe72fbb423100508ff8c8c65
Mohammad Banisaeid <smbl64@gmail.com>
2023-02-20 17:37:31 +0100

    [nvim] Add bootstrapping for packer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
@@ -102,4 +115,9 @@ return require('packer').startup(function(use)
     use 'hrsh7th/nvim-cmp'
 
     use 'j-hui/fidget.nvim'
+
+    -- This must be the last thing
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
