0d26e06835f29f150afd2a3bee270855998d1922
Mohammad Banisaeid <smbl64@gmail.com>
2023-03-17 16:12:41 +0100

    [nvim] Read local vimrc if available

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
 require('me.plugins')
 require('me.config')
 require('me.keybindings')
+
+local vimrc_local = vim.fn.expand("~/.vimrc_local")
+if vim.fn.filereadable(vimrc_local) then
+    vim.cmd('source' .. vimrc_local)
+end
