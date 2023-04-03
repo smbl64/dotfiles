fa5e89f985883256bed8639692617d482da851d7
Mohammad Banisaeid <smbl64@gmail.com>
2023-04-03 15:27:01 +0200

    [nvim] Disable vim regex highlighting

--- nvim/lua/me/config.lua
+++ nvim/lua/me/config.lua
@@ -238,6 +238,7 @@ require('nvim-treesitter.configs').setup {
 
     highlight = {
         enable = true,
+        additional_vim_regex_highlighting = false,
     },
     indent = {
         enable = true,
