6292ab21a2baea87d7966dbcb8d09f7206ed8d1a
Mohammad Banisaeid <smbl64@gmail.com>
2023-04-03 17:36:00 +0200

    [nvim] Detect sql in rust raw_string_literal nodes

--- nvim/after/queries/rust/injections.scm
+++ nvim/after/queries/rust/injections.scm
@@ -4,6 +4,7 @@
 ; Modified version of what's available in ray-x/go.nvim
 ([
    (string_literal)
+   (raw_string_literal)
  ] @sql
  (#match? @sql "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
  (#offset! @sql 0 1 0 -1))
