5b9ea0efaef8053dc9d78e8822af22b8fef21630
Mohammad Banisaeid <smbl64@gmail.com>
2023-08-24 10:57:07 +0200

    [nvim] Extract statusline config to its own module

--- nvim/lua/me/config.lua
+++ nvim/lua/me/config.lua
@@ -392,30 +392,7 @@ for _, lsp in ipairs(servers) do
     }
 end
 
-vim.cmd([[
-    set statusline=
-    set statusline+=%#CursorIM#%{(mode()=='n')?'\ \ NORMAL\ ':''}
-    set statusline+=%#Search#%{(mode()=='i')?'\ \ INSERT\ ':''}
-    set statusline+=%#DiffDelete#%{(mode()=='r')?'\ \ RPLACE\ ':''}
-    set statusline+=%#Cursor#%{(mode()=='v')?'\ \ VISUAL\ ':''}
-    set statusline+=%#Visual#                " colour
-    set statusline+=%{&paste?'\ PASTE\ ':''}
-    set statusline+=%{&spell?'\ SPELL\ ':''}
-    set statusline+=%#CursorIM#              " colour
-    set statusline+=%R                       " readonly flag
-    set statusline+=%M                       " modified [+] flag
-    set statusline+=%#CursorLine#            " colour
-    set statusline+=\ %f\                    " short file name
-    set statusline+=%=                       " right align
-    set statusline+=%#CursorLine#            " colour
-    set statusline+=\ %y\                    " file type
-    set statusline+=%#CursorIM#              " colour
-    set statusline+=%#CursorIM#              " colour
-    set statusline+=\ %3l:%-2c\              " line + column
-    set statusline+=%#CursorIM#              " colour
-    set statusline+=\ %3p%%\                 " percentage
-]])
-
+require("me.statusline")
 
 require("rust-tools").setup({
     server = {
