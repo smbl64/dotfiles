1bdf1dc34298ae20ebeda95956fe702e11ac0803
Mohammad Banisaeid <smbl64@gmail.com>
2023-09-28 14:13:23 +0200

    [nvim] Make fzf-lua use FZF_DEFAULT_COMMAND

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,8 +1,18 @@
+local fzf_cmd = os.getenv("FZF_DEFAULT_COMMAND")
+if fzf_cmd == nil or fzf_cmd == "" then
+    -- Taken from Fzf-Lua docs here
+    -- https://github.com/ibhagwan/fzf-lua/tree/main#default-options
+    fzf_cmd = "--color=never --files --hidden --follow -g '!.git'"
+end
+
 return {
     'ibhagwan/fzf-lua',
     -- optional for icon support
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = {
-        global_git_icons = false,     -- makes it slow on large projects
+        global_git_icons = false, -- makes it slow on large projects
+        files = {
+            cmd = fzf_cmd,
+        }
     }
 }
