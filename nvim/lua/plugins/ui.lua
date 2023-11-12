b166e872ca252553329a71d0926cd66e1de2f7a5
Mohammad Banisaeid <smbl64@gmail.com>
2023-11-12 20:59:27 +0100

    [nvim] Add nvimtree plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -7,6 +7,18 @@ return {
 
     'machakann/vim-highlightedyank',
 
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
+
     -- Fidget shows LSP status updates in bottom right corner
     {
         "j-hui/fidget.nvim",
