e7e7d718790f20ec527cfdbf6229584c1aaf8cf6
Mohammad Banisaeid <smbl64@gmail.com>
2023-11-18 21:10:56 +0100

    [nvim] Disable default mappings for vim-mark

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,16 @@
+-- Disable default mappings set by Vim Mark
+vim.g.mw_no_mappings = 1
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,5 +24,5 @@ require('me.keybindings')
 
 local vimrc_local = vim.fn.expand("~/.vimrc_local")
 if vim.fn.filereadable(vimrc_local) then
-    vim.cmd('source' .. vimrc_local)
+  vim.cmd('source' .. vimrc_local)
 end
