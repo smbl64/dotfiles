82a599dc6b19c6d75803ac6163045ce0e42055bf
Mohammad Banisaeid <smbl64@gmail.com>
2023-12-18 18:02:08 +0100

    [nvim] Add syntax highlighting for caddyfile

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,126 +1,129 @@
 return {
-    -- Markdown
-    'godlygeek/tabular',
-    'preservim/vim-markdown',
+  -- Markdown
+  'godlygeek/tabular',
+  'preservim/vim-markdown',
 
-    -- Working with ollama
-    'David-Kunz/gen.nvim',
+  -- Working with ollama
+  'David-Kunz/gen.nvim',
 
-    'mileszs/ack.vim',
-    'terryma/vim-expand-region',
-    'inkarkat/vim-ingo-library',
-    'inkarkat/vim-mark',
-    'dense-analysis/ale',
-    'Chiel92/vim-autoformat',
-    'michaeljsmith/vim-indent-object',
-    'ludovicchabant/vim-gutentags',
-    'tpope/vim-surround',
-    'tpope/vim-repeat',
-    --Plug 'jiangmiao/auto-pairs'
+  -- Syntax highlighting for caddyfile
+  'isobit/vim-caddyfile',
 
-    'tpope/vim-unimpaired',
-    'kana/vim-textobj-user',
-    'tpope/vim-fugitive',
+  'mileszs/ack.vim',
+  'terryma/vim-expand-region',
+  'inkarkat/vim-ingo-library',
+  'inkarkat/vim-mark',
+  'dense-analysis/ale',
+  'Chiel92/vim-autoformat',
+  'michaeljsmith/vim-indent-object',
+  'ludovicchabant/vim-gutentags',
+  'tpope/vim-surround',
+  'tpope/vim-repeat',
+  --Plug 'jiangmiao/auto-pairs'
 
-    -- Run tests in any language
-    'vim-test/vim-test',
+  'tpope/vim-unimpaired',
+  'kana/vim-textobj-user',
+  'tpope/vim-fugitive',
 
-    -- Python specific
-    'bps/vim-textobj-python',
-    'tmhedberg/SimpylFold',
-    'vim-scripts/indentpython.vim',
-    'smbl64/vim-black-macchiato',
+  -- Run tests in any language
+  'vim-test/vim-test',
 
-    -- 'rust-lang/rust.vim',
-    'mrk21/yaml-vim',
-    'hashivim/vim-terraform',
+  -- Python specific
+  'bps/vim-textobj-python',
+  'tmhedberg/SimpylFold',
+  'vim-scripts/indentpython.vim',
+  'smbl64/vim-black-macchiato',
 
-    'lervag/vimtex',
+  -- 'rust-lang/rust.vim',
+  'mrk21/yaml-vim',
+  'hashivim/vim-terraform',
 
-    -- Web development
-    'pangloss/vim-javascript',
-    'maxmellon/vim-jsx-pretty',
-    'hail2u/vim-css3-syntax',
+  'lervag/vimtex',
 
-    -- Golang
-    {
-        'ray-x/go.nvim',
-        -- Noted: Fixed on this commit until the problem with
-        -- goimports get fixed
-        -- commit = '44bd0589ad22e2bb91f2ed75624c4a3bab0e5f59',
-        config = true,
-    },
+  -- Web development
+  'pangloss/vim-javascript',
+  'maxmellon/vim-jsx-pretty',
+  'hail2u/vim-css3-syntax',
 
-    'rodjek/vim-puppet',
+  -- Golang
+  {
+    'ray-x/go.nvim',
+    -- Noted: Fixed on this commit until the problem with
+    -- goimports get fixed
+    -- commit = '44bd0589ad22e2bb91f2ed75624c4a3bab0e5f59',
+    config = true,
+  },
 
-    -- Lox
-    'timmyjose-projects/lox.vim',
+  'rodjek/vim-puppet',
 
-    -- Dart
-    'dart-lang/dart-vim-plugin',
+  -- Lox
+  'timmyjose-projects/lox.vim',
 
-    -- Zig
-    'ziglang/zig.vim',
+  -- Dart
+  'dart-lang/dart-vim-plugin',
 
-    -- Allow local vimrc files
-    'jenterkin/vim-autosource',
+  -- Zig
+  'ziglang/zig.vim',
 
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate'
-    },
+  -- Allow local vimrc files
+  'jenterkin/vim-autosource',
 
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-    },
-    {
-        "nvim-treesitter/playground",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-        cmd = 'TSPlaygroundToggle'
-    },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate'
+  },
 
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig',
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
+  {
+    "nvim-treesitter/playground",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    cmd = 'TSPlaygroundToggle'
+  },
 
-    'lukas-reineke/lsp-format.nvim',
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig',
 
-    {
-        "L3MON4D3/LuaSnip",
-        version = "2.*",
-        build = "make install_jsregexp",
-        dependencies = { "rafamadriz/friendly-snippets" },
-        config = function()
-            local vscode_loader = require("luasnip.loaders.from_vscode")
+  'lukas-reineke/lsp-format.nvim',
 
-            -- friendly-snippets
-            vscode_loader.lazy_load()
+  {
+    "L3MON4D3/LuaSnip",
+    version = "2.*",
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      local vscode_loader = require("luasnip.loaders.from_vscode")
 
-            -- my custom snippets
-            local path = vim.fn.stdpath "config" .. "/snippets"
-            vscode_loader.lazy_load({ paths = path })
-        end
-    },
+      -- friendly-snippets
+      vscode_loader.lazy_load()
 
-    -- Neovim setup for init.lua and plugin development with full signature help etc
-    'folke/neodev.nvim',
+      -- my custom snippets
+      local path = vim.fn.stdpath "config" .. "/snippets"
+      vscode_loader.lazy_load({ paths = path })
+    end
+  },
 
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = {
-            { 'nvim-lua/plenary.nvim' }
-        }
-    },
+  -- Neovim setup for init.lua and plugin development with full signature help etc
+  'folke/neodev.nvim',
 
-    'simrat39/rust-tools.nvim',
-
-    'folke/which-key.nvim',
-
-    {
-        "NoahTheDuke/vim-just",
-        event = { "BufReadPre", "BufNewFile" },
-        ft = { "\\cjustfile", "*.just", ".justfile" },
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' }
     }
+  },
+
+  'simrat39/rust-tools.nvim',
+
+  'folke/which-key.nvim',
+
+  {
+    "NoahTheDuke/vim-just",
+    event = { "BufReadPre", "BufNewFile" },
+    ft = { "\\cjustfile", "*.just", ".justfile" },
+  }
 }
