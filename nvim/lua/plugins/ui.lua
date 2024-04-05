d0c855b35de5eb0cd514dd4cc8b67ed45c7a6e8e
Mohammad Banisaeid <smbl64@gmail.com>
2024-04-05 13:49:33 +0200

    [nvim] Update plugins

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,30 +1,32 @@
 return {
-    'chriskempson/base16-vim',
-    'NLKNguyen/papercolor-theme',
-    'altercation/vim-colors-solarized',
-    'yasukotelin/shirotelin',
-    'EdenEast/nightfox.nvim',
+  'chriskempson/base16-vim',
+  'NLKNguyen/papercolor-theme',
+  'altercation/vim-colors-solarized',
+  'yasukotelin/shirotelin',
+  'EdenEast/nightfox.nvim',
 
-    'machakann/vim-highlightedyank',
+  'machakann/vim-highlightedyank',
 
-    {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            require("nvim-tree").setup {}
-        end,
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
 
-    -- Fidget shows LSP status updates in bottom right corner
-    {
-        "j-hui/fidget.nvim",
-        tag = "legacy",
-        event = "LspAttach",
-        opts = {
-        },
-    }
+  -- Fidget shows LSP status updates in bottom right corner
+  {
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    event = "LspAttach",
+    opts = {
+    },
+  },
+
+  { 'ray-x/guihua.lua' }
 }
