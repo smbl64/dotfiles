1ce3f8ac961f8fdceca0a96fe77fa6c3fbd50444
Mohammad Banisaeid <smbl64@gmail.com>
2024-07-22 13:12:46 +0200

    [nvim] Remove rg config for fzf-lua

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,18 +1,8 @@
-local fzf_cmd = os.getenv("FZF_DEFAULT_COMMAND")
-if fzf_cmd == nil or fzf_cmd == "" then
-    -- Taken from Fzf-Lua docs here
-    -- https://github.com/ibhagwan/fzf-lua/tree/main#default-options
-    fzf_cmd = "rg --color=never --files --hidden --follow -g '!.git'"
-end
-
 return {
-    'ibhagwan/fzf-lua',
-    -- optional for icon support
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = {
-        global_git_icons = false, -- makes it slow on large projects
-        files = {
-            cmd = fzf_cmd,
-        }
-    }
+  'ibhagwan/fzf-lua',
+  -- optional for icon support
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = {
+    global_git_icons = false, -- makes it slow on large projects
+  }
 }
