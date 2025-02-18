1c69e7b72ce257f5321d9ab3b895a38073fae413
Mohammad Banisaeid <smbl64@gmail.com>
2025-02-18 20:07:04 +0100

    [emacs] Move custom configs to a separate file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,19 +1,6 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(leuven-dark))
- '(custom-safe-themes
-   '("c5692610c00c749e3cbcea09d61f3ed5dac7a01e0a340f0ec07f35061a716436" "dea106ab256a8017a325f51f01b1131915989fa25db48eb831ffb18dac8ecd39" "1d1f4f5b0f792f0bb1b8f944b8ed93b3b20bbebc4ba072c2b7daff82da23ae86" default))
- '(package-selected-packages
-   '(projectile orderless consult marginalia vertico git-gutter evil rust-mode lsp-pyright flycheck company lsp-ui go-mode lsp-mode base16-theme undo-fu evil-collection)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Custom configs go there
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
 
 (setq inhibit-startup-screen t)
 (setq visible-bell nil)
