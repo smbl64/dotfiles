a5c05cd7c221399737c60dfd3356f3c2ee57425f
Mohammad Banisaeid <smbl64@gmail.com>
2025-03-13 22:25:16 +0100

    [nvim] Upgrade plugins

--- nvim/lua/plugins/search.lua
+++ nvim/lua/plugins/search.lua
@@ -4,7 +4,9 @@ return {
     -- optional for icon support
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = {
-      global_git_icons = false, -- makes it slow on large projects
+      defaults = {
+        git_icons = false, -- makes it slow on large projects
+      },
       grep = {
         RIPGREP_CONFIG_PATH = vim.env.RIPGREP_CONFIG_PATH
       }
